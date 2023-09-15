2249b2cd881e2046bcb8b9d11245f935be00b5b1
slowback1 <dwobeck@gmail.com>
2023-09-14 20:40:17 -0500

    Add some basic app styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+
+<main class="main-content">
+  <slot />
+</main>
+
+<style global>
+  @import "../style/reset.css";
+  @import "../style/global.css";
+
+  .main-content {
+      --gutters-y: 32px;
+      --gutters-x: 64px;
+
+      min-height: calc(100vh - var(--gutters-y) * 2);
+      background-color: var(--color-night);
+      color: var(--color-font-light);
+      padding: var(--gutters-y) var(--gutters-x);
+      scroll-behavior: auto;
+  }
+</style>
