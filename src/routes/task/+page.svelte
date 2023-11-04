ff94751b24a02bb8c988ff29e9b58388babb2be1
Andrew Wobeck <dwobeck@gmail.com>
2023-11-04 13:27:14 -0500

    Clean up dead code from recent message bus refactoring

--- src/routes/task/+page.svelte
+++ src/routes/task/+page.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
   import TaskPage from "$lib/pages/TaskPage.svelte";
-  import type IStorageProvider from "$lib/store/IStorageProvider";
-  import { browser } from "$app/environment";
-  import { onMount } from "svelte";
-
-  let storageProvider: IStorageProvider;
-
-  onMount(() => {
-    if (browser)
-      storageProvider = localStorage;
-  });
 </script>
 
-{#if !!storageProvider}
-  <TaskPage storageProvider={storageProvider} />
-{/if}
+<TaskPage />
